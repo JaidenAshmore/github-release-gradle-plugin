fb044adceca868fcb16bf6b14236173372420f0f
Jaiden Ashmore <jaidenkyleashmore@gmail.com>
2020-08-23 08:35:36 +0000

    Prepare for next development iteration with version 0.0.3-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "com.jashmore.gradle"
-version = "0.0.2"
+version = "0.0.3-SNAPSHOT"
 
 val assertJVersion: String by project
 val eclipseGitHubConnectorVersion: String by project
